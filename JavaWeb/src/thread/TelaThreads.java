19e3960692d2a52f7a1e024ce4b89373cdbd6165
Josevan <Josevan@DESKTOP-GV3CQA2>
2022-12-19 11:47:25 -0300

    refazendo as telas sozinho

--- JavaWeb/src/thread/TelaThreads.java
+++ JavaWeb/src/thread/TelaThreads.java
@@ -1,25 +1,16 @@
 package thread;
 
-import java.awt.Dimension;
-
 import javax.swing.JDialog;
 
 public class TelaThreads extends JDialog{
-	public TelaThreads() {
-		setTitle("Minha Tela do Tempo"); //titulo da tela
-		setSize(new Dimension(400, 400)); //tamanho da tela
-		setLocationRelativeTo(null); //centralizar tela
-		setResizable(false); //nao permite a tela ser ajustada de tamanho
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		setVisible(true); //mostrar todo o conteudo da tela
-	}
 
+	public TelaThreads() {
+		setTitle("Tela de Threads");
+		
+		
+		
+		
+		
+		setEnabled(true);
+	}
 }
