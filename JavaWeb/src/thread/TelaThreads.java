b110d8e6c32aece91b600fc70dcfe6544dddb9df
Josevan <Josevan@DESKTOP-GV3CQA2>
2022-12-22 22:05:31 -0300

    AULA - Controlando a Thead 2 com Start e Stop

--- JavaWeb/src/thread/TelaThreads.java
+++ JavaWeb/src/thread/TelaThreads.java
@@ -4,6 +4,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -21,8 +25,39 @@ public class TelaThreads extends JDialog {
 	private JLabel labelTexto1 = new JLabel("Tempo do Campo 2");
 	private JTextField campoTexto1 = new JTextField();
 	
-	private JButton bSalvar = new JButton("Salvar");
+	private JButton bIniciar = new JButton("Start");
 	private JButton bParar = new JButton("Parar");
+	
+	private Runnable threadTempo = new Runnable() {
+		@Override
+		public void run() {
+			while(true) {
+				campoTexto0.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+	
+
+	private Runnable threadTempo1 = new Runnable() {
+		public void run() {
+			while(true) {
+				campoTexto1.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+	
+	private Thread threadTime1;
+	private Thread threadTime2;
 			
 	public TelaThreads()
 	{
@@ -40,7 +75,7 @@ public class TelaThreads extends JDialog {
 		painel1.add(labelTexto0, gridBag);
 		
 		campoTexto0.setPreferredSize(new Dimension(120, 25));
-		campoTexto0.setEnabled(false);
+		campoTexto0.setEditable(false);
 		gridBag.gridx++;
 		painel1.add(campoTexto0, gridBag);
 		
@@ -55,13 +90,40 @@ public class TelaThreads extends JDialog {
 		painel1.add(campoTexto1, gridBag);
 		
 		gridBag.gridy++;
-		bSalvar.setPreferredSize(new Dimension(80, 25));
+		bIniciar.setPreferredSize(new Dimension(80, 25));
 		gridBag.gridx--;
-		painel1.add(bSalvar, gridBag);
+		painel1.add(bIniciar, gridBag);
 		
 		bParar.setPreferredSize(new Dimension(80, 25));
 		gridBag.gridx++;
 		painel1.add(bParar, gridBag);
+		
+		/*Ações dos botoes*/
+		bIniciar.addActionListener(new ActionListener() {	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				threadTime1 = new Thread(threadTempo);	
+				threadTime2 = new Thread(threadTempo1);
+				threadTime1.start();
+				threadTime2.start();
+				
+				bIniciar.setEnabled(false);
+				bParar.setEnabled(true);
+			}
+		});
+		
+		bParar.addActionListener(new ActionListener() {	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				threadTime1.stop();
+				threadTime2.stop();
+				
+				bIniciar.setEnabled(true);
+				bParar.setEnabled(false);
+			}
+		});
+		
+		bParar.setEnabled(false);
 
 		add(painel1);
 		setVisible(true);
