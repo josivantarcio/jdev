069a6976cc354d240ae6251fb5ea4b589d851e11
Josevan T Oliveira <josivan@ubuntu-Inspiron-N4030>
2022-12-26 15:53:55 -0300

    Correção do projeto apos a visita

--- JavaWeb/src/thread/TelaThreads.java
+++ JavaWeb/src/thread/TelaThreads.java
@@ -24,15 +24,10 @@ public class TelaThreads extends JDialog {
 
 	private JLabel labelTexto1 = new JLabel("Tempo do Campo 2");
 	private JTextField campoTexto1 = new JTextField();
-<<<<<<< HEAD
 
-	private JButton bSalvar = new JButton("Salvar");
-=======
-	
 	private JButton bIniciar = new JButton("Start");
->>>>>>> branch 'main' of https://github.com/josivantarcio/jdev.git
 	private JButton bParar = new JButton("Parar");
-<<<<<<< HEAD
+
 
 	private Runnable runThread = new Runnable() {
 		@Override
@@ -47,74 +42,36 @@ public class TelaThreads extends JDialog {
 				}
 			}
 		}
-
-	};
-
-	private Thread threadTempo1;
-	private Thread threadTempo2;
-
-	public TelaThreads() {
-=======
-	
-	private Runnable threadTempo = new Runnable() {
-		@Override
-		public void run() {
-			while(true) {
-				campoTexto0.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
 	};
 	
 
-	private Runnable threadTempo1 = new Runnable() {
-		public void run() {
-			while(true) {
-				campoTexto1.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	};
-	
-	private Thread threadTime1;
-	private Thread threadTime2;
+	private Thread threadTemp;
+	private Thread threadTemp2;
 			
 	public TelaThreads()
 	{
->>>>>>> branch 'main' of https://github.com/josivantarcio/jdev.git
+
 		setTitle("Marcação de Tempo");
-		setSize(new Dimension(400, 200));
+		setSize(new Dimension(550, 350));
 		setLocationRelativeTo(null);
-		setResizable(false);
+		setResizable(true);
 
 		GridBagConstraints gridBag = new GridBagConstraints();
 		gridBag.gridx = 0;
 		gridBag.gridy = 0;
 		gridBag.insets = new Insets(5, 5, 5, 5);
 
-		labelTexto0.setPreferredSize(new Dimension(120, 25));
+		labelTexto0.setPreferredSize(new Dimension(100, 25));
 		painel1.add(labelTexto0, gridBag);
-<<<<<<< HEAD
 
-		campoTexto0.setPreferredSize(new Dimension(200, 25));
-=======
-		
-		campoTexto0.setPreferredSize(new Dimension(120, 25));
->>>>>>> branch 'main' of https://github.com/josivantarcio/jdev.git
+		campoTexto0.setPreferredSize(new Dimension(200,25));
 		campoTexto0.setEditable(false);
 		gridBag.gridx++;
 		painel1.add(campoTexto0, gridBag);
 
+		
+		labelTexto1.setPreferredSize(new DimensionUIResource(100, 25));
 		gridBag.gridy++;
-		labelTexto1.setPreferredSize(new DimensionUIResource(120, 25));
 		gridBag.gridx--;
 		painel1.add(labelTexto1, gridBag);
 
@@ -123,28 +80,26 @@ public class TelaThreads extends JDialog {
 		gridBag.gridx++;
 		painel1.add(campoTexto1, gridBag);
 
-		gridBag.gridy++;
-		bIniciar.setPreferredSize(new Dimension(80, 25));
-		gridBag.gridx--;
-<<<<<<< HEAD
-		painel1.add(bSalvar, gridBag);
 
-=======
+		bIniciar.setPreferredSize(new Dimension(80, 25));
+		gridBag.gridy++;
+		gridBag.gridx--;
 		painel1.add(bIniciar, gridBag);
-		
->>>>>>> branch 'main' of https://github.com/josivantarcio/jdev.git
+
+
 		bParar.setPreferredSize(new Dimension(80, 25));
 		gridBag.gridx++;
 		painel1.add(bParar, gridBag);
+		
 		
 		/*Ações dos botoes*/
 		bIniciar.addActionListener(new ActionListener() {	
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				threadTime1 = new Thread(threadTempo);	
-				threadTime2 = new Thread(threadTempo1);
-				threadTime1.start();
-				threadTime2.start();
+				threadTemp = new Thread(runThread);	
+				threadTemp2 = new Thread(runThread);
+				threadTemp.start();
+				threadTemp2.start();
 				
 				bIniciar.setEnabled(false);
 				bParar.setEnabled(true);
@@ -154,8 +109,8 @@ public class TelaThreads extends JDialog {
 		bParar.addActionListener(new ActionListener() {	
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				threadTime1.stop();
-				threadTime2.stop();
+				threadTemp.stop();
+				threadTemp2.stop();
 				
 				bIniciar.setEnabled(true);
 				bParar.setEnabled(false);
@@ -163,26 +118,6 @@ public class TelaThreads extends JDialog {
 		});
 		
 		bParar.setEnabled(false);
-
-		bSalvar.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				threadTempo1 = new Thread(runThread);
-				threadTempo1.start();
-				threadTempo2 = new Thread(runThread);
-				threadTempo2.start();
-			}
-		});
-		
-		bParar.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				threadTempo1.stop();				
-				threadTempo2.stop();
-			}
-		});
 
 		add(painel1);
 		setVisible(true);
